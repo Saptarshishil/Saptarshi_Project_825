2fc1a6e3d77ec85037c0e009e36a3cd85b46d58e
Saptarshishil <125542432+Saptarshishil@users.noreply.github.com>
2024-08-19 15:26:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 # from flask import Flask, render_template, jsonify, request
 # from flask_pymongo import PyMongo
 
@@ -73,3 +74,6 @@ def qa():
 
 
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
